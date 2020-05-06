07bf52c29e1d68ad2c62c13b3de4812cc1e3677a
Scroller55 <62955263+Scroller55@users.noreply.github.com>
2020-05-06 17:05:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,202 +1,51 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, View, TouchableHighlight, Dimensions, StyleSheet, Image } from 'react-native';
-import Constants from 'expo-constants';
+import { StyleSheet, Text, Keyboard,  View, TextInput, TouchableWithoutFeedback, Alert, KeyboardAvoidingView, Platform,Button, Image, TouchableHighlight,} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import Homescreen from './src/login/homescreen.js';
+import Popsmoke from './src/login/ps.js';
+import Cameronboyce from './src/login/cb.js';
+import Juicewrld from './src/login/jw.js';
+import Kobebryant from './src/login/kb.js';
+import Nipseyhussle from './src/login/nh.js';
+import Tupacshakur from './src/login/ts.js';
+import Lilpeep from './src/login/lp.js';
+import Xxxtentacion from './src/login/xt.js';
+import Eazye from './src/login/ee.js';
 
-let deviceHeight = Dimensions.get('window').height;
-let deviceWidth = Dimensions.get('window').width;
+
+
+
+
+
+
+
+
+
+
+
+const RootStack = createStackNavigator(
+{
+  Home: { screen: Homescreen },
+  Popsmoke: { screen: Popsmoke },
+   Cameronboyce: { screen: Cameronboyce },
+   Juicewrld: { screen: Juicewrld },
+   Kobebryant: { screen: Kobebryant },
+   Nipseyhussle: { screen: Nipseyhussle },
+   Tupacshakur: { screen: Tupacshakur },
+   Lilpeep: { screen: Lilpeep },
+   Xxxtentacion: { screen: Xxxtentacion },
+   Eazye: { screen: Eazye },
+   
+},
+{
+    initialRouteName: 'Home',
+}
+
+);
 
 export default class App extends Component {
-state={
-  basicinfoPageDisplay:'block',
-  experiencePageDisplay:'none',
-}
-handleBasicinfoPagePress  = () =>this.setState(state=>({
-basicinfoPageDisplay:'block',
-experiencePageDisplay:'none',
-}));
-handleExperiencePagePress  = () =>this.setState(state=>({
-  basicinfoPageDisplay:'none',
-experiencePageDisplay:'block',
-}));
-render() {
-return (
-<View style={styles.container}>
-<Text style={styles.title}>
-Welcome to Bio App
-</Text>
-<View style={styles.navContainer}>
-<TouchableHighlight style={styles.navButton}
-onPress={this.handleBasicinfoPagePress}>
-<Text style={styles.navButtonText}>
-Basic Info
-</Text>
-</TouchableHighlight>
-<TouchableHighlight style={styles.navButton}
-onPress={this.handleExperiencePagePress}>
-<Text style={styles.navButtonText}>
-Pop Smoke
-</Text>
-</TouchableHighlight>
-</View>
-<View style={styles.contentContainer}>
-<View style ={{display:this.state.basicinfoPageDisplay}}>
-<View style={styles.mainConatiner}>
-<View style={styles.searchButton}>
-    <Text style={styles.textinSearch}>
-      Pop Smoke
-    </Text>
-  </View>
-  <View style={styles.searchButton}>
-    <Text style={styles.textinSearch}>
-      Cameron Boyce
-    </Text>
-  </View>
-  <View style={styles.searchButton}>
-    <Text style={styles.textinSearch}>
-      Juice Wrld
-    </Text>
-  </View>
-  <View style={styles.searchButton}>
-    <Text style={styles.textinSearch}>
-      KObe Bryant
-    </Text>
-  </View>
-  <View style={styles.searchButton}>
-    <Text style={styles.textinSearch}>
-      Nipsey Hussle
-    </Text>
-  </View>
-  <View style={styles.searchButton}>
-    <Text style={styles.textinSearch}>
-      Tupac Shakur
-    </Text>
-  </View>
-  <View style={styles.searchButton}>
-    <Text style={styles.textinSearch}>
-      Lil Peep
-    </Text>
-  </View>
-  <View style={styles.searchButton}>
-    <Text style={styles.textinSearch}>
-      XXXTentacion
-    </Text>
-  </View>
-  <View style={styles.searchButton}>
-    <Text style={styles.textinSearch}>
-      Eazy E
-    </Text>
-  </View>
-</View>
-</View>
-<View style ={{display: this.state.experiencePageDisplay}}>
-<View style={styles.mainConatiner}>
-<Text style ={styles.text3}>
-Pop Smoke
-</Text>
-<Text style ={styles.text2}>
-Born: July 20, 1999, Brooklyn, New York, NY
-</Text>
-<Text style ={styles.text2}>
-Died: February 19, 2020, Cedars-Sinai Medical Center, Los Angeles, CA
-</Text>
-<Text style ={styles.text2}>
-Occupation:   American rapper, singer and songwriter.
-</Text>
-</View>
-</View>
-</View>
-</View>
-);
-}
+  render() {
+    return <RootStack />;
+  }
 }
 
-
-const styles = StyleSheet.create({
-container: {
-height: deviceHeight,
-width: deviceWidth,
-
-},
-contentContainer: {
-height: 5*(deviceHeight/5),
-width: deviceWidth,
-backgroundColor: 'green',
-},
-navContainer: {
-height: deviceHeight/7,
-width: deviceWidth,
-backgroundColor: 'green',
-flexDirection: 'row',
-alignItems: 'center',
-justifyContent: 'center',
-borderTopWidth: 10,
-borderColor: 'white',
-},
-navButton: {
-height: deviceHeight/14,
-width: deviceWidth/4,
-backgroundColor: 'white',
-borderColor: 'lightblue',
-borderWidth: 2,
-alignItems: 'center',
-justifyContent: 'center',
-margin: 20,
-},
-navButtonText: {
-fontSize: deviceHeight/40,
-textAlign: 'center',
-color: 'blue',
-},
-mainConatiner:{
-backgroundColor:'blue ',
-textAlign:'center',
-justifyContent:'center',
-alignItems:'center',
-fontSize:70,
-},
-title:{
-    color: 'darkblue',
-     fontSize: 30,
-         fontWeight: 'bold',
-},
-paragraph: {
-fontFamily: 'Didot',
-color: 'white',
-fontSize: 15,
-textAlign: 'center',
-},
-
-text2: {
-fontFamily: 'Didot',
-color: 'black',
-fontSize: 14,
-textAlign: 'center',
-         fontWeight: 'bold',
-
-},
-text1: {
-fontFamily: 'Didot',
-color: 'white',
-fontSize: 15,
-textAlign: 'center',
-},
-searchButton:{
-  height: 50,
-  width: 110,
-  justifyContent: 'center',
-  borderColor: 'black',
-  borderWidth:3,
-},
-textinSearch:{
-  fontSize: 16,
-  fontWeight: 'bold',
-  fontColor: 'white',
-  textAlign: 'center',
-},
-text3: {
-fontFamily: 'Didot',
-color: 'red',
-fontSize: 25,
-textAlign: 'center',
-},
-});
